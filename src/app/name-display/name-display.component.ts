0c8f5164bda574426be46e0a1d7c9878c820b310
Your name here <kumaripreetti@gmail.com>
2022-04-08 11:09:11 +0530

    angular app

---
+++ src/app/name-display/name-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-display',
+  templateUrl: './name-display.component.html',
+  styleUrls: ['./name-display.component.css']
+})
+export class NameDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
